26c31a529ffcffef8be3a2f44c4d4fd22aa47897
Pavel <rabota.pashok@gmail.com>
2024-04-12 21:51:28 +0300

    gitignore back

--- config/gulp-tasks/gitignore.js
+++ config/gulp-tasks/gitignore.js
@@ -7,16 +7,12 @@ export const gitignore = () => {
 		fs.appendFile('./.gitignore', 'flsStartTemplate/\r\n', cb)
 		fs.appendFile('./.gitignore', 'node_modules/\r\n', cb)
 		fs.appendFile('./.gitignore', '.gitignore\r\n', cb)
-		// fs.appendFile('./.gitignore', 'dist/\r\n', cb)
+		fs.appendFile('./.gitignore', 'dist/\r\n', cb)
 		fs.appendFile('./.gitignore', 'Source/\r\n', cb)
 		fs.appendFile('./.gitignore', 'version.json\r\n', cb)
 		fs.appendFile('./.gitignore', app.buildFolder + '\r\n', cb)
 		fs.appendFile('./.gitignore', '**/*.zip\r\n', cb)
 		fs.appendFile('./.gitignore', '**/*.rar\r\n', cb)
-		fs.appendFile('./.gitignore', 'src/\r\n', cb)
-		fs.appendFile('./.gitignore', 'config/\r\n', cb)
-		fs.appendFile('./.gitignore', 'gulpfile.js\r\n', cb)
-		fs.appendFile('./.gitignore', 'package.json\r\n', cb)
 		//if (projectName !== 'flsStart') del('./.git/');
 	}
 	return app.gulp.src(`${app.path.srcFolder}`)
